efc23651c766c65cfaab6248ca2baed854f1b034
Sharon Ben Shushan <102518563+Sharonbs94@users.noreply.github.com>
2022-07-04 21:00:28 +0300

    OnlineCasinoData

---
+++ INSERT_OnlineCasinoData.sql
@@ -0,0 +1,89 @@
+USE OnlineCasinoPj
+GO
+
+------------------Employees Table----------------------
+INSERT INTO Employees (First_Name, Last_Name, Job_Title, Email, Phone, Address, City, Country, HireDate, BirthDate)
+VALUES
+('Christopher', 'Schmid','Support' ,'kikosch@gmail.com', '+41738311392', 'Bachmattstrasse 45 st','Zurich', 'Switzerland','2010/04/07','1987/10/3'),
+('Elizabeth', 'Support','Account Man\ger' ,'whiteliz@gmail.com', '+41797564381', 'Eisfeldstrasse 9 st','Zurich', 'Switzerland','2010/12/15', '1980/05/17'),
+('Anderson', 'Timoty','Support' ,'timo_t@gmail.com', '+41732397111', 'Bederstrasse 51 st','Zurich', 'Switzerland','2011/07/22', '1985/04/07'),
+('Roxana', 'Moonasky','Support' ,'roxsky@gmail.com', '+41722123383', 'Gartenstrasse 16 st','Zurich', 'Switzerland','2011/12/17', '1982/07/17'),
+('Ter', 'Stegen','Support' ,'tersty9@gmail.com', '+41700572081', 'Gutenbergstrasse 6 st','Zurich', 'Switzerland','2013/01/01', '1990/12/12'),
+('Moriah', 'Bless','Support' ,'blessymo@gmail.com', '+41738608211', 'Holzgasse 19 st','Zurich', 'Switzerland','2013/09/18', '1991/01/10'),
+('Christopher', 'Juneshmid','Support' ,'shmidnt2@gmail.com', '+41739946513', 'Freigutstrasse 10 st','Zurich', 'Switzerland','2014/03/04', '1988/02/18'),
+('Timor', 'Verner','Support' ,'vernert@gmail.com', '+41767384983', 'Bachmattweg 8 st','Zurich', 'Switzerland','2015/05/08', '1990/12/16'),
+('Christina', 'Muller','Support' ,'c_muller@gmail.com', '+41738187083', 'Kleinbühlstrasse 13 st','Zurich', 'Switzerland','2020/02/19', '1995/09/11'),
+('Rikki', 'Robertsen','Support' , 'rrobertsenx@nasa.gov', '+41429961459', 'Zurich', 'Switzerland', '08205 Park Meadow Lane','2020/02/18', '1994/01/23');
+GO
+
+
+------------------PlayersDetails Table----------------------
+INSERT INTO PlayerDetails (Password, FirstName, LastName, AddressPl, Email, Country, Gender, BirthDate)
+VALUES
+( 'tx22g3', 'Christoforo', 'Moryson',  '049 Glendale Trail','srowdell0@jimdo.com', 'Portugal', 'Male', '1987/03/15'),
+('KKCAKo', 'Jose', 'Talkington', '16807 Forest Junction', 'mdarinton1@wufoo.com' ,'Portugal', 'Male', '1992/01/06'),
+('oKBAk', 'Sonni', 'Munsey',  '177 Almo Court', 'jlacotte2@icio.us','Portugal', 'Female', '1973/10/29'),
+('JyXfck3', 'Mattheus', 'Froschauer', '8118 Homewood Crossing','nfishlee3@geocities.jp','United States', 'Male', '1984/01/01'),
+('IuKnO', 'Cherise', 'Abrahamson',  '4 Saint Paul Park','bwilflinger4@mediafire.com' ,'Portugal', 'Female', '1994/06/07'),
+('u93iM', 'Henrik', 'Rottenbury', '3180 Anderson Parkway','agrcic5@wordpress.com','Portugal', 'Male', '1989/11/11'),
+('3w9w8f', 'Norby', 'Derwin', '68 Spenser Circle',  'lkaley6@odnoklassniki.ru','Portugal', 'Male', '1982/11/06'),
+('uM8l0Z', 'Berkly', 'Lathom', '1723 Lighthouse Bay Parkway','kluckham7@microsoft.com', 'Portugal', 'Male', '1979/12/24'),
+('9bc56L', 'Lenee', 'Ernshaw',  '9428 Fair Oaks Court','jadan8@cam.ac.uk', 'Portugal', 'Female', '1976/12/07'),
+('jy69JI', 'Robby', 'Rouf', '00 Nova Park','ahorick9@ca.gov', 'United States', 'Male', '1977/06/30'),
+('upVV1R', 'Ingamar', 'Butterwick', '38 Ohio Way','gcansdalea@hhs.gov','United States', 'Male', '1975/09/30'),
+('erIEQU', 'Arther', 'Kunzler',  '988 Lakeland Pass','jlochb@hhs.gov', 'Portugal', 'Male', '1971/12/08'),
+('exUBsj', 'Judie', 'Buckell', '15 Division Terrace','yorkneyc@tuttocitta.it','United States', 'Female', '1995/01/16'),
+('Nm3zGC', 'Clayson', 'Laurentino', '83 Spohn Drive','lcleared@whitehouse.gov','United States', 'Male', '1973/08/10'),
+('IfxfBh', 'Claude', 'Tafani', '6 Drewry Lane','areglere@dmoz.org', 'United States', 'Female', '1978/03/16'),
+('R0u4mJ', 'Butch', 'Chisholme', '3 Grayhawk Parkway','gbredesf@sakura.ne.jp', 'Portugal', 'Male', '1988/12/24'),
+('Qu2Iig', 'Baxie', 'Allgood', '901 Heffernan Terrace','fmateug@taobao.com','Portugal', 'Male', '1979/12/29'),
+('Ryiq', 'Kale', 'Geerits', '1 Packers Park','gbunkerh@imgur.com','United States', 'Male', '1987/09/05'),
+('RyuTz3S', 'Brion', 'Shoebotham', '760 Graedel Trail','amcspirroni@google.com.br', 'Portugal', 'Male', '1990/03/15'),
+('CdDdP6', 'Greggory', 'Alben',  '9345 Heath Pass','asalkildj@gov.uk', 'United States', 'Male', '1980/04/26'),
+('CVz46g', 'Neddy', 'Hanner',  '64 Becker Junction', 'nshorbrookk@dagondesign.com', 'United States', 'Male', '1990/09/17'),
+('K6J9f', 'Cordell', 'Gradon',  '3 Moose Terrace', 'hadamczewskil@wisc.edu', 'Portugal', 'Male', '1975/09/22'),
+('rLbfw', 'Matteo', 'Crews',  '518 Lotheville Court', 'dchaddertonm@webeden.co.uk', 'United States', 'Male', '1975/02/12'),
+('pgUkxi', 'Didi', 'Jowling',  '4420 Declaration Court','swestnedgen@issuu.com', 'United States', 'Female', '1985/01/04'),
+('Kk6JbA', 'Candice', 'Jorez', '90137 Continental Hill','dmyrkuso@apache.org', 'Portugal', 'Female', '1974/01/15'),
+('4PsylcV', 'Silvana', 'Guy', '264 Lotheville Avenue','fpawlatap@purevolume.com', 'Portugal', 'Female', '1974/10/05'),
+('uUXpuM', 'Ariana', 'Verney', '242 Delladonna Plaza','cschaffelq@cloudflare.com','Portugal', 'Female', '1975/10/04'),
+('QrpFIG', 'Abran', 'Jerrans',  '438 Towne Place', 'gdissmanr@auda.org.au', 'United States', 'Male', '1979/05/28'),
+('fvj4L', 'Jordan', 'De Malchar', '749 Esker Parkway', 'cblasis@ucla.edu', 'United Kingdom', 'Male', '1994/05/26'),
+('7OAc8v', 'Nada', 'O''Sirin', '264 Lien Trail', 'lkhotlert@about.com', 'Portugal', 'Female', '1974/08/19');
+GO
+
+
+------------------Support Table----------------------
+INSERT INTO Support (EmployeeID, PlayerID, Status, AlertCase, AlertDate)
+VALUES
+ (6, 6, 'Not Solved', 'Cant take away money', '2021/04/02'),
+ (3, 20, 'Not Solved', 'Cant take away money', '2021/05/01'),
+ (8, 10, 'Not Solved', 'Cant take away money', '2021/05/07'),
+ (10, 21, 'Solved', 'Cant Deposit', '2021/06/22'),
+ (5, 19, 'Not Solved', 'Cant take away money', '2021/11/02'),
+ (7, 4, 'Not Solved', 'cant Log In', '2021/12/09'),
+ (4, 2, 'Solved', 'cant Log In', '2021/12/18'),
+ (5, 3, 'Not Solved', 'cant Log In', '2022/01/16'),
+ (7, 26, 'Not Solved', 'cant Log In', '2022/02/04'),
+ (1, 15, 'Solved', 'Cant Deposit', '2022/04/05');
+ GO
+
+------------------GameType_DESC Table----------------------
+INSERT INTO GameDetails (GameName, Description) VALUES 
+('Slot Machine','In the slot machine there are 3 wheels, and there are a total of 6 unique symbols. Only if the same
+symbol appears on all 3 wheels, the player wins. Otherwise, the player loses'),
+('Blackjack','Card game whose object is to be dealt cards having a higher count than those of the dealer, up to but not exceeding 21. 
+The dealer may use a single deck of 52 cards or two or more decks from a holder called a shoe.
+Aces count as 1 or 11, and face cards as 10. Depending on the rules used, bets may be placed before the deal, after each player has been dealt one card facedown, 
+or after each player has received two cards facedown and the dealer has exposed one of his cards.');
+GO
+
+
+
+------------------Game_Stat Table----------------------
+--INSERT INTO Game_Stat (GameDate, Number_Of_Rouds, WinNO, Total_Bets_Amount, Total_Winning_Amount) VALUES 
+--(1,'2022/05/19' , 225 , 102, 1263754 ,956587),
+--(2,'2022/05/19' ,186 , 96, 968845 ,420675);
+--GO
+
+
